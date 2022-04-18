f84e32dff487c461d0b8f5fe1e59ca670541ccb0
Ganesh Kumar <ganesh@authorselvi.com>
2022-04-18 14:16:51 +0530

    Changes

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,4 +1,3 @@
-import { useState,useEffect } from 'react';
 import './card-list.css';
 import './card-list-img.jsx';
 
--- src/components/search-box/search-box.component.jsx
+++ src/components/search-box/search-box.component.jsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+
 import './search-box.css';
 
 const SearchBox = (props) => {
